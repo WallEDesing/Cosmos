96cf1baf5079a5d6e1bb5467434b375487d6bce2
WallEDesing <walledesing2k01@gmail.com>
2023-04-08 00:44:15 +0430

    Create the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>cosmos</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helper/theme.js
@@ -0,0 +1,5 @@
+export const theme ={
+    primary:"#F2F2F2",
+    secondary:"#EE1C6F",
+
+}
